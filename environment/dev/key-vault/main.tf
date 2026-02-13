f64c6c97730a9ef8cc0e8713b25e5af3cc1c66fa
manasma-k <manasmallick25@gmail.com>
2026-02-13 09:30:04 +0000

    commit

--- environment/dev/key-vault/main.tf
+++ environment/dev/key-vault/main.tf
@@ -1,7 +1,7 @@
 module "foundation" {
   source = "git::ssh://git@github.com/manasma-k/foundation.git//modules/key-vault?ref=CHANGE_ME"
 
-  name                = var.name
+  name                = manas
   resource_group_name = var.resource_group_name
   location            = var.location
   tenant_id           = var.tenant_id
