9f7d731ba7b0a708f27ac0707cb59d19bf221085
manasma-k <manasmallick25@gmail.com>
2026-02-18 11:45:53 +0000

    commit

--- environment/dev/foundation/main.tf
+++ environment/dev/foundation/main.tf
@@ -3,7 +3,7 @@ module "vnet" {
 
   resource_group_name = var.resource_group_name
   location            = var.location
-  vnet_name           = var.vnet_name
+  name                = var.vnet_name
   address_space       = var.address_space
   tags                = var.tags
 }
