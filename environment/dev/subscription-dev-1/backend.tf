f1b7b125b7ac2672d2d930a15ef457a671f4ca22
manasma-k <manasmallick25@gmail.com>
2026-02-27 11:55:37 +0000

    commit

--- environment/dev/subscription-dev-1/backend.tf
+++ environment/dev/subscription-dev-1/backend.tf
@@ -2,11 +2,12 @@ terraform {
 	required_version = ">= 1.5.0"
 
 	required_providers {
-		azurerm = {
-			source  = "hashicorp/azurerm"
-			version = "~> 3.100"
-		}
-	}
+    azurerm = {
+      source                = "hashicorp/azurerm"
+      version               = ">=3.00.0"
+      configuration_aliases = [azurerm.remote,azurerm.remote2,azurerm.remote3, azurerm.remote4, azurerm.remote5, azurerm.remote6, azurerm.remote7, azurerm.remote8]
+    }
+  }
 
 	backend "azurerm" {}
 }
