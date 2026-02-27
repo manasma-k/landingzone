4835c23c2e779340994a06db4903706812beb73e
manasma-k <manasmallick25@gmail.com>
2026-02-27 12:05:30 +0000

    commit

--- environment/dev/subscription-dev-1/main.tf
+++ environment/dev/subscription-dev-1/main.tf
@@ -13,6 +13,7 @@ module "base" {
 
 	providers = {
 		azurerm         = azurerm
+		azurerm.remote  = azurerm.remote
 		azurerm.remote2 = azurerm.remote2
 		azurerm.remote3 = azurerm.remote3
 		azurerm.remote4 = azurerm.remote4
