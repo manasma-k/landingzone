4201c5f10f95dcddb3e06b2a919968cf7edd1da6
manasma-k <manasmallick25@gmail.com>
2026-03-02 11:16:12 +0000

    commit

--- environment/dev/subscription-dev-1/backend.tf
+++ environment/dev/subscription-dev-1/backend.tf
@@ -15,27 +15,27 @@ terraform {
 provider "azurerm" {
 	alias    = "remote"
 	features {}
-	subscription_id = 4eddb456-1696-47e9-b24c-825f989a3704
+	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 provider "azurerm" {
 	alias    = "remote1"
 	features {}
-	subscription_id = 4eddb456-1696-47e9-b24c-825f989a3704
+	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 provider "azurerm" {
 	alias    = "remot2"
 	features {}
-	subscription_id = 4eddb456-1696-47e9-b24c-825f989a3704
+	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 provider "azurerm" {
 	alias    = "remote3"
 	features {}
-	subscription_id = 4eddb456-1696-47e9-b24c-825f989a3704
+	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 provider "azurerm" {
 	alias    = "remote4"
 	features {}
-	subscription_id = 4eddb456-1696-47e9-b24c-825f989a3704
+	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 variable "subscription_id" {
 	type    = string
