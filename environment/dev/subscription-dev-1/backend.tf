7539875e960afe5c88865587a3606f16b234308b
manasma-k <manasmallick25@gmail.com>
2026-03-02 11:19:50 +0000

    commit

--- environment/dev/subscription-dev-1/backend.tf
+++ environment/dev/subscription-dev-1/backend.tf
@@ -23,7 +23,7 @@ provider "azurerm" {
 	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
 provider "azurerm" {
-	alias    = "remot2"
+	alias    = "remote2"
 	features {}
 	subscription_id = "4eddb456-1696-47e9-b24c-825f989a3704"
 }
