a0eb74eb54e9f31f09dd4446ff7345bb56501a72
manasma-k <manasmallick25@gmail.com>
2026-03-03 12:33:51 +0000

    commit

--- environment/dev/subscription-dev-2/main.tf
+++ environment/dev/subscription-dev-2/main.tf
@@ -12,11 +12,11 @@ locals {
 module "application_gateway" {
   source = "../../../modules/application-gateway-base"
 
-  name                                 = app-gateway-test
-  resource_group_name                  = rg-dev-network
-  location                             = eastus
-  subnet_id                            = /subscriptions/4eddb456-1696-47e9-b24c-825f989a3704/resourceGroups/rg-dev-network/providers/Microsoft.Network/virtualNetworks/vnet_subscription_dev_1/subnets/snet_workload
-  private_frontend_ip_address          = 10.10.1.9
+  name                                 = "app-gateway-test"
+  resource_group_name                  = "rg-dev-network"
+  location                             = "eastus"
+  subnet_id                            = "/subscriptions/4eddb456-1696-47e9-b24c-825f989a3704/resourceGroups/rg-dev-network/providers/Microsoft.Network/virtualNetworks/vnet_subscription_dev_1/subnets/snet_workload"
+  private_frontend_ip_address          = "10.10.1.9"
   ssl_certificate_secret_id            = var.ssl_certificate_secret_id
   backend_ip_addresses                 = var.backend_ip_addresses
   backend_port                         = var.backend_port
